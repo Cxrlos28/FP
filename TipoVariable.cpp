8223aeeddc44c5854fdfd6cbcef6fe84c20de732
Cxrlos28 <carlos.tableros35@my.unitec.edu.mx>
2024-09-26 19:42:26 -0600

    TipoVariable.cpp

---
+++ TipoVariable.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std; //
+
+
+int main()
+{
+    // 1. Entero (int)
+    int numeroEntero = 13;
+
+    cout << "Entero : " << numeroEntero << endl;
+
+    // 2. Entero corto (short int)
+    short int numeroCorto = 32000;
+    
+    cout << "Entero corto : " << numeroCorto << endl;
+
+    // 3. Entero Largo (long int) 
+    long long numeroLargo = 5564673707L;
+    cout << "Entero largo : " << numeroLargo << endl;
+
+    // 4. Numero sin signo short
+    unsigned short int numeroCortoSinSigno = 31999;
+    cout << "numeroCortoSinSigno : " << numeroCortoSinSigno << endl;
+
+    // 5. Numero sin signo 
+    unsigned numeroSinSigno = 2254673656; 
+    cout << "numerosinsigno : " << numeroSinSigno << endl; 
+
+    // 6. Numero largo sin signo
+    unsigned long long NumeroLargosinSigno = 76768768968976;
+    cout << "Numerolargosinsigno : " << NumeroLargosinSigno << endl; 
+}
